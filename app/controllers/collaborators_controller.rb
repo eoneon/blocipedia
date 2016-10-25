df64b53a9b00455d45b6692b5830edb121119bb4
Eon <eoneon123@gmail.com>
2016-10-24 19:30:19 -0700

    updated collaborator policy: create and destroy

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -4,21 +4,21 @@ class CollaboratorsController < ApplicationController
     @collaborator = Collaborator.new
     @collaborator.user_id = params[:collaborator][:user_id]
     @collaborator.wiki = @wiki
+    authorize @collaborator
 
-    if CollaboratorPolicy.new(current_user, @wiki, @collaborator)
-      if @collaborator.save
-        flash[:notice] = "Added a collaborator to this wiki."
-      else
-        flash[:alert] = "Adding collaborator failed."
-      end
+    if @collaborator.save
+      flash[:notice] = "Added a collaborator to this wiki."
+    else
+      flash[:alert] = "Adding collaborator failed."
     end
+
     redirect_to :back
   end
 
   def destroy
-    collaborator = Collaborator.find(params[:id])
-
-    if collaborator.destroy
+    @collaborator = Collaborator.find(params[:id])
+    authorize @collaborator
+    if @collaborator.destroy
       flash[:notice] = "Collaborator removed."
     else
       flash[:alert] = "Removing collaborator failed."
--- app/policies/collaborator_policy.rb
+++ app/policies/collaborator_policy.rb
@@ -1,26 +1,13 @@
 class CollaboratorPolicy
-  attr_reader :user, :wiki, :collaborator
+  attr_reader :user, :collaborator
 
-  def initialize(user, collaborator, wiki)
+  def initialize(user, collaborator)
     @user = user
-    @wiki = wiki
     @collaborator = collaborator
   end
 
-  def index?
-    true
-  end
-
-  def new?
-    true
-  end
-
   def create?
-    @user == current_user && @wiki.private && @user.role == 'premium'
-  end
-
-  def edit?
-    create?
+    @collaborator.wiki.private && @user.role == 'premium'
   end
 
   def destroy?
